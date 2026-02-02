92be3615246d80f0de6e89854046e9ece29847e6
kpoptrashBlaenk <aldinmusik@gmail.com>
2026-02-02 14:54:37 +0100

    feat(task): finalize task module

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
+import { Task, TaskSchema } from './task.schema'
 import { TaskService } from './task.service'
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Task.name, schema: TaskSchema }]),
+  ],
+  controllers: [TaskController],
   providers: [TaskService],
 })
 export class TaskModule {}
